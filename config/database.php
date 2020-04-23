54a6ec5951b54f0fe9505257393970c2c39435f9
samajaloleru <samsonajaloleru@gmail.com>
2020-04-23 12:05:45 +0100

    commit

--- config/database.php
+++ config/database.php
@@ -1,9 +1,10 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 
 return [
-    $DATABASE_URL=parse_url(‘postgres://axstpzppwmthsb:9cd119c40c2d1f66b46e63b24ea71e2614d601a07f3964adf7a0d28e438bf0ea@ec2-18-233-137-77.compute-1.amazonaws.com:5432/d61dnsmt75nrum’);
+    '$DATABASE_URL' =>parse_url("postgres://axstpzppwmthsb:9cd119c40c2d1f66b46e63b24ea71e2614d601a07f3964adf7a0d28e438bf0ea@ec2-18-233-137-77.compute-1.amazonaws.com:5432/d61dnsmt75nrum"),
 
     /*
     |--------------------------------------------------------------------------
@@ -66,11 +67,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => $DATABASE_URL("host"),
-            'port' => $DATABASE_URL('DB_PORT', '5432'),
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
             'database' => ltrim($DATABASE_URL["path"],"/"),
-            'username' => $DATABASE_URL("user"),
-            'password' => $DATABASE_URL("pass"),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pass"],
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
