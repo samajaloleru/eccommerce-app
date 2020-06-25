44409ecfdd2f0426aef282b1b5b1c1cb04d229a1
samajaloleru <samsonajaloleru@gmail.com>
2020-06-25 23:36:14 +0100

    test

--- config/database.php
+++ config/database.php
@@ -67,8 +67,8 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => $DATABASE_URL("host"),
-            'port' => $DATABASE_URL("port"),
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
             'database' => ltrim($DATABASE_URL["path"], "/"),
             'username' => $DATABASE_URL["user"],
             'password' => $DATABASE_URL["pass"],
